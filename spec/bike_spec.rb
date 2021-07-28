bbdb342bff9893a197995cf87d63c0af17c3f78f
Ahsan <rasul.ahsan@gmail.com>
2021-07-28 15:30:03 +0100

    created a bike spec and written passing code

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,3 +1,13 @@
 require 'docking_station.rb'
 describe Bike do
+  it { expect(Bike.new.working?).to eq (true)}
 end
+
+  # it 'release bike' do
+  #   docking_station = DockingStation.new
+  #   expect(docking_station.release_bike).to eq ('release bike')
+  # end
+  #it { expect(DockingStation.new.release_bike).to eq ('release bike')}
+#end
+
+# it { expect(docking_station.release_bike).to eq ('release bike')}
