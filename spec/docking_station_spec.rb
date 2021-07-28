40435236f5fc2094025c5ba6cfcc3ae943a05f4d
Corinne <corinne.boesch@gmail.com>
2021-07-28 14:57:47 +0100

    One line it statement

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,11 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  it 'release bike' do
-    docking_station = DockingStation.new
-    expect(docking_station.release_bike).to eq ('release bike')
-  end
+  # it 'release bike' do
+  #   docking_station = DockingStation.new
+  #   expect(docking_station.release_bike).to eq ('release bike')
+  # end
+  it { expect(DockingStation.new.release_bike).to eq ('release bike')}
 end
+
+# it { expect(docking_station.release_bike).to eq ('release bike')}
